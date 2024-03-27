0cf79b1a20c91ce8bba5d2784089752f431e7c28
Danilo Costa Bento <91625647+Dann074@users.noreply.github.com>
2024-03-27 11:54:10 -0300

    Update day5(CleanCode).js

--- Javascript/day5(CleanCode).js
+++ Javascript/day5(CleanCode).js
@@ -1,34 +1,57 @@
-let fruits = [];
-let dairy = [];
-let sweets = [];
-let frozen = [];
-let food = "";
-let category = "";
+let fruits = []
+let frozen = []
+let dairy = []
+let food = []
+let sweets = []
+let welcome = ""
+let option = ""
+let name = ""
 
-let addMore = "yes";
-while(addMore != "no"){
-    addMore = prompt("Do you want to add one item in itens list? Answer 'yes' or 'no'.");
-    while (addMore != "yes" && addMore != "no") {
-	alert(`Invalid entry!`);
-        addMore = prompt("Do you want to add more itens? Answer 'yes' or 'no'.");
-    }
-	
-    if (addMore === "no"){
-        break;
-    }
-	
-    food = prompt("What food do you want to add?");
-    category = prompt("Which category does this food fit into: 'fruit', 'dairy', 'sweets' or 'frozen'?");
-    if(category === 'fruits'){
-        fruits.push(food);
-    } else if (category === 'dairy'){
-        dairy.push(food);
-    } else if (category === 'sweets'){
-        sweets.push(food);
-    } else if (category === 'frozen'){
-        frozen.push(food);
-    } else {
-        alert("This category was not pre-defined.")
-    }
-}
-alert(`Shopping list:\n  Fruits: ${frutas}\n  Dairy: ${laticinios}\n  Sweets: ${doces}\n  Frozen: ${congelados}`);
+do {
+  welcome = prompt(`Welcome to your list. What you want to do?
+  \n1- Add food
+  \n2- Exit
+  \nFood:
+  \n${food}`)
+  switch (welcome) {
+    case "1":
+      do {
+        option = prompt(`What type of food you want to add?
+      \n1- Fruit
+      \n2- Frozen
+      \n3- Dairy
+      \n4- Sweets
+      \n5- Exit`)
+        switch (option) {
+          case "1":
+            name = prompt(`What's the name of the fruit?`)
+            fruits.push(name)
+            break
+          case "2":
+            name = prompt(`What's the name of the frozen?`)
+            frozen.push(name)
+            break
+          case "3":
+            name = prompt(`What's the name of the dairy?`)
+            dairy.push(name)
+            break
+          case "4":
+            name = prompt(`What's the name of the sweet?`)
+            sweets.push(name)
+            break
+          case "5":
+            alert(`Exiting...`)
+            break
+          default:
+            alert(`Invalid entry, please retry`)
+        }
+        food = [`Fruits: ${fruits}`, `\nFrozens: ${frozen}`, ` \nDairys: ${dairy}`, `\nSweets: ${sweets}`]
+      } while (option != "5");
+      break;
+    case "2":
+        alert(`Exiting...`)
+        break
+    default:
+      alert(`Invalid entry...`)
+      } 
+} while (welcome != 2)
